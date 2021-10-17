657b6182d5df829266861430352fcce6ff0a3565
Dhairya Chavand <87813664+dhairyachavand@users.noreply.github.com>
2021-10-17 22:01:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,9 +30,9 @@ const checkWin = () => {
 
     wins.forEach(e =>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "") ){
-
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won"
-            isgameover = true;
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
         }
     })
 }
